cbd8d10ea7cd7b1ba70c1a4cc520e4342447a610
Mark Florence <mflo999@gmail.com>
2018-03-23 06:18:07 -0400

    Cleanup

--- main.js
+++ main.js
@@ -3,26 +3,17 @@ const {
   BrowserWindow
 } = require('electron');
 
-let mainWindow;
+let theWindow;
 
-// Executes when the application
-// is initialized.
 app.on('ready', function () {
-  console.log('Starting application!');
-  // Create browser window
-  // with given parameters
-  mainWindow = new BrowserWindow({});
-  mainWindow.loadURL('http://localhost:4200');
-
-  // It is useful to open dev tools
-  // for debug.
-  mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', function () {
-    mainWindow = null;
+  theWindow = new BrowserWindow({frame: 'false'});
+  theWindow.loadURL('http://localhost:4200');
+  theWindow.setMenu(null);
+  theWindow.on('closed', function () {
+    theWindow = null;
   });
 });
 
-// Defines the behavior on close.
 app.on('window-all-closed', function () {
   app.quit();
 });

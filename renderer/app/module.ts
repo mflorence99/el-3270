689a5b8715f43e8bcb133fd7fcd0f05c966c4039
Mark Florence <mflo999@gmail.com>
2018-03-25 10:03:57 -0400

    Experimental changes

--- renderer/app/module.ts
+++ renderer/app/module.ts
@@ -1,22 +1,50 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { EL3270Component } from './pages/el3270';
+import { LibModule } from 'ellib/lib/module';
 import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
+
+/**
+ * el-3270 module definition
+ */
+
+const COMPONENTS = [
+  EL3270Component
+];
+
+const MODULES = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  CommonModule,
+  LibModule,
+  NgxElectronModule,
+  RouterModule
+];
+
+const ROUTES: Routes = [ ];
+
+const SERVICES = [ ];
 
 @NgModule({
 
   bootstrap: [EL3270Component],
 
   declarations: [
-    EL3270Component
+    ...COMPONENTS
   ],
 
   imports: [
-    BrowserModule,
-    NgxElectronModule
+    ...MODULES,
+    RouterModule.forRoot(ROUTES)
   ],
 
-  providers: []
+  providers: [
+    ...SERVICES
+  ]
 
 })
 

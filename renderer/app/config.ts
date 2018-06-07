aa9c083e14343c297da4f368b4ade08c50bc1d6c
Mark Florence <mflo999@gmail.com>
2018-06-07 04:56:06 -0400

    https://github.com/mflorence99/el-3270/issues/2

---
+++ renderer/app/config.ts
@@ -0,0 +1,15 @@
+/**
+ * Common configuration settings
+ */
+
+export class Config {
+
+  fontSizeThrottle = 250;
+  setBoundsThrottle = 250;
+
+  portMax = 65535;
+  portMin = 23;
+
+}
+
+export const config = new Config();
